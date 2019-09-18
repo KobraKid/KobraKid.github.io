c828076dba636cc3975b3cdaff68ecb6b8e1b6b6
Michael Huyler <michaelhuyler@gmail.com>
2019-09-18 00:47:06 -0500

    Stylized bars

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-// Make the page take up 100% width
-$("body").css("padding-top", $(".navbar").outerHeight())
-$("#scrollbar").height($(window).height() - $(".navbar").outerHeight());
-
-// Set up tooltips
-$('[data-toggle="tooltip"]').tooltip();
